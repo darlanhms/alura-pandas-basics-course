3a28fadc116e0086824102600a564e6e7f8962a1
darlanhms <darlanhermessjb@hotmail.com>
2025-02-09 18:33:16 -0300

    Add decision tree classifier implementation and visualization to bank marketing analysis

--- bank_marketing.py
+++ bank_marketing.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 data = pd.read_csv("./marketing_investimento.csv")
 
@@ -48,4 +50,28 @@ dummy = DummyClassifier()
 dummy.fit(x_trained, y_trained)
 
 dummy.score(x_test, y_test)
+
+# %%
+tree_classifier = DecisionTreeClassifier(random_state=5, max_depth=3)
+tree_classifier.fit(x_trained, y_trained)
+
+tree_classifier.predict(x_test)
+
+tree_classifier.score(x_trained, y_trained)
+
+column_names = ['casado (a)',
+                'divorciado (a)',
+                'solteiro (a)',
+                'fundamental',
+                'medio',
+                'superior',
+                'inadimplencia',
+                'fez_emprestimo',
+                'idade',
+                'saldo',
+                'tempo_ult_contato',
+                'numero_contatos']
+
+plt.figure(figsize=(15, 6))
+plot_tree(tree_classifier, filled=True, class_names=['nao', 'sim'], fontsize=7, feature_names=column_names);
 # %%
