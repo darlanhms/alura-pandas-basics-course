daeceab7215bb68e7abfad5bb4a71c1bc7a48ebe
darlanhms <darlanhermessjb@hotmail.com>
2025-02-09 14:27:20 -0300

    Add outlier detection and removal in data cleaning script; include seaborn for visualization

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,8 @@
+#%%
 import pandas as pd
 import json
 import numpy as np
+import seaborn as sns
 
 #%%
 with open("./dataset-telecon.json") as dataset:
@@ -53,3 +55,53 @@ no_null_df = no_empty_data.dropna(subset=["conta.contrato", "conta.faturamente_e
 no_null_df.reset_index(drop=True, inplace=True)
 
 # %%
+sns.boxplot(x=no_null_df['cliente.tempo_servico'])
+
+# %% Identifying outliers
+Q1 = no_null_df['cliente.tempo_servico'].quantile(.25)
+Q3 = no_null_df['cliente.tempo_servico'].quantile(.75)
+IQR = Q3 - Q1
+lower_limit = Q1 - 1.5 * IQR
+higher_limit = Q3 + 1.5 * IQR
+
+outlier_filter = (no_null_df["cliente.tempo_servico"] < lower_limit) | (no_null_df["cliente.tempo_servico"] > higher_limit)
+
+no_outliers_df = no_null_df.copy()
+
+# %%
+no_outliers_df.loc[outlier_filter, "cliente.tempo_servico"] = np.ceil(
+    no_outliers_df.loc[outlier_filter, "conta.cobranca.Total"] /  no_outliers_df.loc[outlier_filter, "conta.cobranca.mensal"]
+)
+
+# %% Removing outliers
+Q1 = no_outliers_df['cliente.tempo_servico'].quantile(.25)
+Q3 = no_outliers_df['cliente.tempo_servico'].quantile(.75)
+IQR = Q3 - Q1
+lower_limit = Q1 - 1.5 * IQR
+higher_limit = Q3 + 1.5 * IQR
+
+outlier_filter = (no_outliers_df["cliente.tempo_servico"] < lower_limit) | (no_outliers_df["cliente.tempo_servico"] > higher_limit)
+
+# %%
+no_outliers_df = no_outliers_df[~outlier_filter]
+
+no_outliers_df.reset_index(drop=True, inplace=True)
+
+# %% 
+no_id_df = no_outliers_df.drop("id_cliente", axis=1).copy()
+
+# %%
+mapper = {
+    'nao': 0,
+    'sim': 1,
+    'masculino': 0,
+    'feminino': 1,
+}
+
+columns = ['telefone.servico_telefone', 'Churn', 'cliente.parceiro', 'cliente.dependentes', 'conta.faturamente_eletronico', 'cliente.genero']
+
+# %%
+no_id_df[columns] = no_id_df[columns].replace(mapper)
+
+# %%
+df_dummies = pd.get_dummies(no_id_df).copy()
