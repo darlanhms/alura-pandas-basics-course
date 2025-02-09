82e0acfe8fcb4f8eb6fb5df06c892935007bfd5c
darlanhms <darlanhermessjb@hotmail.com>
2025-02-09 19:10:27 -0300

    Add new datasets and update data paths; implement KNN classifier and model serialization

--- bank_marketing.py
+++ bank_marketing.py
@@ -2,12 +2,14 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
-data = pd.read_csv("./marketing_investimento.csv")
+data = pd.read_csv("./data/marketing_investimento.csv")
 
 #%%
 px.histogram(data, x="fez_emprestimo", text_auto=True, color="aderencia_investimento", barmode="group")
@@ -43,21 +45,21 @@ label_encoder = LabelEncoder()
 y_transformed = label_encoder.fit_transform(y)
 
 # %% Data separation
-x_trained, x_test, y_trained, y_test = train_test_split(x_transformed, y_transformed, stratify=y, random_state=5)
+x_train, x_test, y_train, y_test = train_test_split(x_transformed, y_transformed, stratify=y, random_state=5)
 
 # %%
 dummy = DummyClassifier()
-dummy.fit(x_trained, y_trained)
+dummy.fit(x_train, y_train)
 
 dummy.score(x_test, y_test)
 
 # %%
 tree_classifier = DecisionTreeClassifier(random_state=5, max_depth=3)
-tree_classifier.fit(x_trained, y_trained)
+tree_classifier.fit(x_train, y_train)
 
 tree_classifier.predict(x_test)
 
-tree_classifier.score(x_trained, y_trained)
+tree_classifier.score(x_train, y_train)
 
 column_names = ['casado (a)',
                 'divorciado (a)',
@@ -74,4 +76,54 @@ column_names = ['casado (a)',
 
 plt.figure(figsize=(15, 6))
 plot_tree(tree_classifier, filled=True, class_names=['nao', 'sim'], fontsize=7, feature_names=column_names);
+
+# %%
+scaler = MinMaxScaler()
+x_train_normalized = scaler.fit_transform(x_train)
+x_test_normalized = scaler.transform(x_test)
+
+pd.DataFrame(x_train_normalized)
+
+# %%
+knn = KNeighborsClassifier()
+knn.fit(x_train_normalized, y_train)
+
+knn.score(x_test_normalized, y_test)
+
+# %%
+print(f'Dummy accuracy: {dummy.score(x_test, y_test)}')
+print(f'Tree accuracy: {tree_classifier.score(x_test, y_test)}')
+print(f'KNN accuracy: {knn.score(x_test_normalized, y_test)}')
+
+# %%
+with open("model_onehotenc.pkl", 'wb') as file:
+    pickle.dump(one_hot, file)
+
+# %%
+with open("model_tree_classifier.pkl", 'wb') as tree_file:
+    pickle.dump(tree_classifier, tree_file)
+
+# %%
+new_data = {
+    'idade': [45],
+    'estado_civil':['solteiro (a)'],
+    'escolaridade':['superior'],
+    'inadimplencia': ['nao'],
+    'saldo': [23040],
+    'fez_emprestimo': ['nao'],
+    'tempo_ult_contato': [800],
+    'numero_contatos': [4]
+}
+
+new_data = pd.DataFrame(new_data)
+
+one_hot_model = pd.read_pickle("./model_onehotenc.pkl")
+tree_classifier_model = pd.read_pickle("./model_tree_classifier.pkl")
+
+# %%
+normalized_new_data = one_hot_model.transform(new_data)
+
+normalized_new_data
+tree_classifier_model.predict(normalized_new_data)
+
 # %%
--- carbon_emission.py
+++ carbon_emission.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-gas_emissions = pd.read_excel("./1-SEEG10_GERAL-BR_UF_2022.10.27-FINAL-SITE.xlsx", sheet_name="GEE Estados")
+gas_emissions = pd.read_excel("./data/1-SEEG10_GERAL-BR_UF_2022.10.27-FINAL-SITE.xlsx", sheet_name="GEE Estados")
 
 # %%
 print(gas_emissions.head())
